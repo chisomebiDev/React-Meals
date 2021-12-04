433b1dfb702c1426c16fabb7e6a6d1812dbecafb
chisomebi onwunyi <onwunyichisom@gmail.com>
2021-12-04 17:22:41 +0100

    Added meals image

--- src/components/Navbar/Header.js
+++ src/components/Navbar/Header.js
@@ -2,6 +2,7 @@ import Cart from "./Cart";
 import styles from "./Header.module.css";
 import Modal from "../Modal/Modal";
 import { useState } from "react";
+import meals from "../../assets/meals.jpg";
 
 function Header() {
   const [showModal, setShowModal] = useState(false);
@@ -16,6 +17,9 @@ function Header() {
         <h1 className={styles.heading}>ReactMeals</h1>
         <Cart onClick={showModalHandler} />
       </header>
+      <div className={styles.mainImage}>
+        <img src={meals} alt="A table of food placed on a table" />
+      </div>
       {showModal && <Modal onClick={showModalHandler} />}
     </>
   );
