85bb519f48edbf9f642b1261dad85c3af683e6d2
chisomebi onwunyi <onwunyichisom@gmail.com>
2021-12-06 22:28:57 +0100

    Added cart animation

--- src/components/Navbar/Cart.js
+++ src/components/Navbar/Cart.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/modalArrayContext";
 import styles from "./Cart.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,15 +7,35 @@ import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 const cartIcon = <FontAwesomeIcon icon={faShoppingCart} />;
 
 function Cart(props) {
+  const [btnHighlighted, setBtnHighlighted] = useState(false);
+
   const cartCtx = useContext(CartContext);
 
+  const { items } = cartCtx;
+
+  const btnClass = `${styles.cart} ${btnHighlighted ? styles.bump : ""}`;
+
   //Calculate total items in cart
-  const total = cartCtx.items.reduce((cum, cur) => {
+  const total = items.reduce((cum, cur) => {
     return cum + +cur.quantFood;
   }, 0);
 
+  //Css animation
+  useEffect(() => {
+    if (items.length === 0) return;
+    setBtnHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={styles.cart} onClick={props.onClick}>
+    <button className={btnClass} onClick={props.onClick}>
       {cartIcon} Your Cart {"   "}
       <span className={styles.items}>{total || 0}</span>
     </button>
